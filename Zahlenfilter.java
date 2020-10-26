22d340c8a897e05850931d2fd656905f28aefec4
repl.it user <replituser@example.com>
2020-10-26 14:40:09 +0000

    ass.1 first commit

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -1,7 +1,5 @@
-class Zahlenfilter{
+class Zahlenfilter {
   public static void main(String[] args) {
-    for (int ZAHL = 1; ZAHL < 21; ZAHL++) {
-      System.out.println(ZAHL);
-    }
+    System.out.println("Hello Irene!");
   }
 }
