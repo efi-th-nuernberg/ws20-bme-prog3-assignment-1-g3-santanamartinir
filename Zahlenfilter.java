a2b79f3fe2bf7eb2e839f624b5f236102b75a2a7
repl.it user <replituser@example.com>
2020-10-26 15:02:19 +0000

    ass.1 second and last commit

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -1,5 +1,27 @@
 class Zahlenfilter {
   public static void main(String[] args) {
-    System.out.println("Hello Irene!");
+    
+    int ZAHL;
+
+    for (ZAHL = 1; ZAHL < 201; ZAHL++) {
+
+      if(ZAHL % 5 == 0) {
+      System.out.println(ZAHL + " ist durch 5 teilbar!");
+      }
+
+      if(ZAHL % 10 == 9) {
+      System.out.println(ZAHL + " endet auf 9!");
+      }
+      
+      int VORGAENGER = ZAHL--;
+      int ERGEBNIS = ZAHL + VORGAENGER;
+
+      if(ERGEBNIS % 3 == 0){
+        System.out.println(
+        ZAHL + " und " + VORGAENGER + " addiert ergeben " + ERGEBNIS + " und " + ERGEBNIS + " ist durch 3 teilbar");
+      }
+
+      //System.out.println(ZAHL);
+    }
   }
 }
